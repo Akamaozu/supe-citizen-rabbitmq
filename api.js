55fe92add505519a7b0c7c8fd3cccbaa5d428107
Uzo Olisemeka <uzo@designbymobius.ca>
2020-04-21 04:05:46 -0400

    api: add hooks, run hook when new item enters queue, log and drop item if no handler is found

--- api.js
+++ api.js
@@ -1,3 +1,6 @@
+var create_task = require('cjs-task'),
+    create_hook = require('cjs-sync-hooks');
+
 module.exports = function( citizen, rabbitmq_citizen_name ) {
   if( ! citizen ) throw new Error( 'citizen required to create api instance' );
   if( ! rabbitmq_citizen_name ) rabbitmq_citizen_name = 'rabbitmq';
@@ -12,6 +15,8 @@ module.exports = function( citizen, rabbitmq_citizen_name ) {
   api.consume_queue = consume_queue;
   api.create_exchange = create_exchange;
   api.publish_to_exchange = publish_to_exchange;
+
+  api.hook = create_hook();
 
   return api;
 
@@ -37,9 +42,67 @@ module.exports = function( citizen, rabbitmq_citizen_name ) {
       config = {};
     }
 
-    var request_args = { queue_id: queue_id, config: config };
+    var queue_creation = create_task();
 
-    citizen.request.send( rabbitmq_citizen_name, 'create-queue', request_args, callback );
+    queue_creation.callback( function( error ){
+      if( error ) return callback( error );
+
+      var created_queue = queue_creation.get( 'created-queue' );
+      if( ! created_queue ) return callback( new Error( 'created queue details not found' ) );
+      else return callback( null, created_queue );
+    });
+
+    queue_creation.step( 'send creation request to rabbitmq citizen', function(){
+      var request_args = { queue_id: queue_id, config: config };
+
+      citizen.request.send( rabbitmq_citizen_name, 'create-queue', request_args, function( error, created_queue ){
+        if( error ) return queue_creation.end( error );
+
+        queue_creation.set( 'created-queue', created_queue );
+        queue_creation.next();
+      });
+    });
+
+    queue_creation.step( 'define behavior when new item enters queue', function(){
+
+      citizen.noticeboard.watch( rabbitmq_citizen_name +'-queue-'+ queue_id +'-new-item', 'handle-new-item', function( msg ){
+        var details = msg.notice,
+            item_id = details.item_id,
+            item = details.item,
+            mail = item,
+            mail_handled = false;
+
+        api.hook.run( rabbitmq_citizen_name +'-'+ queue_id +'-mail-received', handle_mail, mail );
+
+        if( ! mail_handled ){
+          console.log( 'action=handle-'+ rabbitmq_citizen_name +'-'+ queue_id +'-mail success=false reason="no handler for mail found" mail=', mail );
+          return ack_mail( item_id );
+        }
+
+        function handle_mail(){
+          if( mail_handled ) return;
+
+          mail_handled = true;
+
+          api.hook.end();
+
+          return function(){
+            ack_mail( item_id );
+          }
+        }
+
+        function ack_mail( item_id, config ){
+
+          api.ack_item( item_id, config, function( error ){
+            if( error ) console.log( 'action=ack-'+ rabbitmq_citizen_name +'-'+ queue_id +'-item success=false reason="'+ error.message + '"' );
+          });
+        }
+      });
+
+      queue_creation.next();
+    });
+
+    queue_creation.start();
   }
 
   function consume_queue( queue_id, options, callback ){
